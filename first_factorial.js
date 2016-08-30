66106d80a82e1f3ff9022b5981aa9aaec69890cf
Timothy Crane <mickeymouth82@gmail.com>
2016-08-30 16:41:36 -0400

    palindrome

--- first_factorial.js
+++ first_factorial.js
@@ -6,9 +6,10 @@
 //FirstFactorial should be a function that takes the 'num' parameter being passed to it and returns the factorial of that parameter.
 
 function FirstFactorial(num) { 
+
   var factorial = 1;
   for (var i = 1; i <= num; i++) {
-      factorial = factorial * i;
+    factorial = factorial * i;
   }
   return factorial;   
-};
+}
---
+++ palindrome.js
@@ -0,0 +1,15 @@
+//---- Palindrome ----//
+
+//The function Palindrome should recieve the 'str' parameter being passed and return true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return false. 
+//For example: "racecar" is also "racecar" backwards.
+
+function Palindrome(str) { 
+  var revStr = "";
+  for (var i = str.length -1; i >= 0; i--) {
+     revStr += str[i];
+  } 
+  if (str.replace(/ /g, '') === revStr.replace(/ /g, '')) {
+    return true;
+  }
+  return false;      
+}
