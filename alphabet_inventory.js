5f309b154c03fc20425344ecf9d8d29d595892c3
Tim Crane <mickeymouth82@gmail.com>
2016-10-07 13:32:10 -0500

    Update alphabet_inventory.js

--- alphabet_inventory.js
+++ alphabet_inventory.js
@@ -3,9 +3,9 @@
 //The function alphaCount should take as parameters an alphabet and a text. 
 //alphaCount should tally the count of each letter defined in said alphabet (case insensitive), and return the result of this tally.
 //For example: 		alphabet: |  text:	               |  output:
-//					"aBc"     | "aabbccdd"             |  "a:2,b:2,c:2"
-//                  "x"       | "X is my friend"       |  "x:1"            
-//                  "hue_3"   | "anlecxkac.gpaoo._43_" |  "e:1,_:2,3:1"
+//					      "aBc"     | "aabbccdd"             |  "a:2,b:2,c:2"
+//                "x"       | "X is my friend"       |  "x:1"            
+//                "hue_3"   | "anlecxkac.gpaoo._43_" |  "e:1,_:2,3:1"
 
 function alphaCount (alphabet, text) {
   alphabet = alphabet.toLowerCase();
