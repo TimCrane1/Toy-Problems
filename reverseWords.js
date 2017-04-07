b07808c44c41c45e97db15a68f5391426a5a1781
TimCrane <mickeymouth82@gmail.com>
2017-04-07 14:03:58 -0500

    take that word down flip it and reverse it

--- reverseWords.js
+++ reverseWords.js
@@ -1,1 +1,11 @@
 //---- Reverse Words ----//
+
+//reverse the word
+
+function reverseWords(str) {
+  var result = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    result+= str[i];
+  }
+  return result;
+} 
