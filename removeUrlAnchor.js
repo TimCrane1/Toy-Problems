1d7e06d78fa9ab1a82adee3f9a3058812f0c2a7b
Timothy Crane <mickeymouth82@gmail.com>
2017-04-17 13:58:59 -0500

    remove anchor

---
+++ removeUrlAnchor.js
@@ -0,0 +1,16 @@
+//---- Remove Anchor ----//
+
+//The function removeUrlAnchor returns the url with anything after the anchor (#) removed
+
+function removeUrlAnchor(url){
+  
+  let result = '';
+  //iterate over the input url pushing each character to the result as long as the current index is not '#'
+  for (let i = 0; i < url.length; i++) {
+    if (url[i] === '#') {
+      return result;
+    }
+    result+= url[i];
+  }
+  return result;
+}
