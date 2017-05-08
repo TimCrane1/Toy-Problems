b10d00ac6bc387a6bfa27c77b74075e3ea127e33
Timothy Crane <mickeymouth82@gmail.com>
2017-05-08 13:33:15 -0500

    balance

---
+++ balance.js
@@ -0,0 +1,10 @@
+//---- Balance ----//
+
+// The function balance reccieves as arguments two strings consisting of exclamation points and question marks, a right and a left.
+// Each exclamation point weight is 2; Each question mark weight is 3.
+// If the left side is more heavier, balance should return "Left"; If the right side is heavier, "Right". If they are balanced, "Balance".
+// For Example:
+// balance("!!","??") === "Right"
+// balance("!??","?!!") === "Left"
+// balance("!!???!????","??!!?!!!!!!!") === "Balance"
+
