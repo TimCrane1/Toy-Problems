bed0f98e3efe6f925435c71b56582d3a1152eb02
Timothy Crane <mickeymouth82@gmail.com>
2017-05-08 13:45:07 -0500

    balance using reduce

--- balance.js
+++ balance.js
@@ -1,6 +1,6 @@
 //---- Balance ----//
 
-// The function balance reccieves as arguments two strings consisting of exclamation points and question marks, a right and a left.
+// The function balance recieves as arguments two strings consisting of exclamation points and question marks, a right and a left.
 // Each exclamation point weight is 2; Each question mark weight is 3.
 // If the left side is more heavier, balance should return "Left"; If the right side is heavier, "Right". If they are balanced, "Balance".
 // For Example:
@@ -8,3 +8,22 @@
 // balance("!??","?!!") === "Left"
 // balance("!!???!????","??!!?!!!!!!!") === "Balance"
 
+function balance(left,right){
+  let rightSum = 0;
+  let leftSum = 0;
+  for (let i = 0; i < left.length; i++) {
+    left[i] === "!" ? leftSum+= 2 : leftSum+= 3;
+  }
+  for (let i = 0; i < right.length; i++) {
+    right[i] === "!" ? rightSum+= 2 : rightSum+= 3;
+  }
+  return leftSum === rightSum ? "Balance" : leftSum > rightSum ? "Left" : "Right";
+}
+
+// Using the built-in array method 'Reduce'
+
+function balance(left,right) {
+  const leftSum = left.split("").reduce((a, b) => a + ((b === "!") ? 2 : 3), 0);
+  const rightSum = right.split("").reduce((a, b) => a + ((b === "!") ? 2 : 3), 0);
+  return leftSum === rightSum ? "Balance" : leftSum > rightSum ? "Left" : "Right";
+}
