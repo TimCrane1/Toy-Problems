c0560199d3e1ab549e2c9dd2c78c53ff9cb641f5
Timothy Crane <mickeymouth82@gmail.com>
2017-05-10 10:46:05 -0500

    number pyramid

---
+++ numberPyramid.js
@@ -0,0 +1,33 @@
+//---- Number Pyramid ----//
+
+// The function numberPyramid should create a pyramid pattern with up to n(parameter) number of rows.
+// If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+// All the lines in the pattern have same length i.e equal to the number of characters in the last line.
+// If n is a two digit integer only the 2nd digit will be represented in the pyramid so as to maintain an increment of one digit between lines 9 and 10, etc.
+// Examples:
+
+//pattern(5):
+
+//     1    
+//    121   
+//   12321  
+//  1234321 
+// 123454321
+
+// pattern(15):
+
+//               1              
+//              121             
+//             12321            
+//            1234321           
+//           123454321          
+//          12345654321         
+//         1234567654321        
+//        123456787654321       
+//       12345678987654321      
+//      1234567890987654321     
+//     123456789010987654321    
+//    12345678901210987654321   
+//   1234567890123210987654321  
+//  123456789012343210987654321 
+// 12345678901234543210987654321
